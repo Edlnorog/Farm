823cb1f8c86f5d2ef68f04e52d573ad601fffd16
romhalfer <119695331+Edlnorog@users.noreply.github.com>
2023-01-24 14:08:23 +0300

    eightth commit
    place fix

--- Assets/Script/Building.cs
+++ Assets/Script/Building.cs
@@ -11,13 +11,16 @@ public class Building : MonoBehaviour
 {
     public GameObject buildingToPlace;
     private float posx, posy;
-    public float[] xm=new float[10] , ym=new float[10] ;
-    private static int k=0,f=0;
+    public float[] xm=new float[100] , ym=new float[100] ;
+    private static int f=0;
+    private static float okr,okri;
+   
 
     //GameObject.FindWithTag("Player").tarnsorm.position 
     private void Start()
     {
         //Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); //Выводим только время
+        
     }
     public void build()
     {
@@ -25,23 +28,67 @@ public class Building : MonoBehaviour
         posy = GameObject.FindWithTag("Player").transform.position.y;
         
         
-        
-        
-        
+
+        for (int i = 0; i < xm.Length; i++)
+        {
+            if (Math.Abs(posx - xm[i]) < 0.3f & xm[i]!=0)
+            {
+                f = 1; 
+                break;
+            }
+        }
+
         if (Run.px < 0)
         {
-            Instantiate(buildingToPlace, new Vector3(posx - 0.3f, posy, 0), Quaternion.identity);
-            xm[k] = posx-0.3f;
+            
+            if (f != 1)
+            {
+                for (int i = 0; i < xm.Length; i++)
+                {
+                    Instantiate(buildingToPlace, new Vector3(posx - 0.3f, posy, 0), Quaternion.identity);
+                    if (xm[i] == 0)
+                    {
+                        
+                        xm[i] = posx + 0.3f;
+                        ym[i] = posy + 0.3f;
+                        break;
+                        
+                    }
+                 
+                }
                 
+            }
+        
+            
                 
         }
+
         if (Run.px > 0)
         {
-            Instantiate(buildingToPlace, new Vector3(posx + 0.3f, posy, 0), Quaternion.identity);
-            xm[k] = posx - 0.3f;
-        }
-                
-       k++;
 
+            if (f != 1)
+            {
+
+                for (int i = 0; i < xm.Length; i++)
+                {
+                    Instantiate(buildingToPlace, new Vector3(posx + 0.3f, posy, 0), Quaternion.identity);
+                    if (xm[i] == 0)
+                    {
+
+                        xm[i] = posx - 0.3f;
+                        ym[i] = posy - 0.3f;
+                        break;
+
+                    }
+
+                }
+
+            }
+        }        
+        
+        
+        f = 0;
     }
+   
+   
 }    
