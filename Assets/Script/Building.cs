b81ee4e03b68b6b0d82ad23cab03058d1a295be7
romhalfer <119695331+Edlnorog@users.noreply.github.com>
2023-02-06 11:10:05 +0300

    win

--- Assets/Script/Building.cs
+++ Assets/Script/Building.cs
@@ -11,21 +11,21 @@ public class Building : MonoBehaviour
 {
     public GameObject buildingToPlace;
     private float posx, posy;
-    public float[] xm=new float[100] , ym=new float[100] ;
-    private static int f=0,k=0;
-    private static float okr,okri;
+    public float[] xm = new float[100], ym = new float[100];
+    private static int f = 0, k = 0;
+    private static float okr, okri;
     public GameObject closest;
     public GameObject[] gg;
     public string nearest;
-    public Vector3 fff,yu,ui;
+    public Vector3 fff, yu, ui;
     //Instantiate(buildingToPlace, new Vector3(posx - 0.3f, posy, 0), Quaternion.identity);
     //Instantiate(buildingToPlace, new Vector3(posx + 0.3f, posy, 0), Quaternion.identity);
     //GameObject.FindWithTag("Player").tarnsorm.position 
     void Start()
     {
         //Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); //Выводим только время
-        
-        
+
+
     }
     public void build()
     {
@@ -33,29 +33,30 @@ public class Building : MonoBehaviour
         posy = GameObject.FindWithTag("Player").transform.position.y;
         gg = GameObject.FindGameObjectsWithTag("Finish");
         //GameObject.FindWithTag("Finish").transform.parent = GameObject.FindWithTag("Respawn").transform;
-        nearest= Findclothestenemy().name;
-        yu = Findclothestenemy().transform.position;
+        //nearest= Findclothestenemy().name;
+        //yu = Findclothestenemy().transform.position;
         Debug.Log(nearest);
         nearest = null;
 
 
         if (Run.px < 0)
         {
-            
-            
-            
-                
+
+            Instantiate(buildingToPlace, new Vector3(posx - 0.5f, posy, 0), Quaternion.identity).transform.parent = GameObject.FindWithTag("Respawn").transform; ;
+
+
         }
 
         if (Run.px > 0)
         {
 
-            
+
         }
         Debug.Log(gg);
-        k = 0;
+        k++;
         f = 0;
     }
+    /*
     GameObject Findclothestenemy()
     {
         float distance = Mathf.Infinity;
@@ -83,3 +84,5 @@ public class Building : MonoBehaviour
     }
     
 }    
+    */
+}
